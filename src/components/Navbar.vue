b042e6c1167f58e2a4356adee30211ad90afbc4f
Tosan Krisna <31754768+tosankrisna@users.noreply.github.com>
2021-10-27 21:49:03 +0800

    change padding size in large screen

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="bg-primary">
-    <nav class="container flex justify-between py-6 lg:py-4 px-3 xl:px-0">
+    <nav class="container flex justify-between py-6 lg:py-4 px-3 lg:px-10 xl:px-0">
       <div class="flex items-center gap-4 md:gap-6 lg:gap-12">
         <span class="flex md:hidden"><i class="fas fa-bars text-white text-2xl" @click="$emit('toggleSidebar')"></i></span>
         <router-link to="/"><span class="text-white text-xl lg:text-2xl font-semibold">Movieku</span></router-link>
